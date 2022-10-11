afaf0fd5caea3abc7144851ed94d10986ded0a8a
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-11 13:09:55 +0500

    Get all tasks and start schedular

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::time::Duration;
-
-use crate::schedular::task::task_options::{DailyTaskOptoins, TaskOptions};
 use dotenv::dotenv;
 use schedular::schedular::Schedular;
-use tokio::time::{sleep_until, Instant};
+use tasks::get_tasks;
 
 mod schedular;
 mod tasks;
@@ -13,54 +10,8 @@ async fn main() {
     dotenv().ok();
     let mut my_schedular = Schedular::new();
 
-    async fn my_runner2() {
-        sleep_until(Instant::now() + Duration::new(3, 0)).await;
-        println!("HI2");
+    for (runner, options) in get_tasks() {
+        my_schedular.every_day(runner, Some(options));
     }
-
-    async fn my_runner1() {
-        sleep_until(Instant::now() + Duration::new(20, 0)).await;
-        println!("MY");
-    }
-    async fn my_runner() {
-        sleep_until(Instant::now() + Duration::new(45, 0)).await;
-        println!("HI");
-    }
-
-    my_schedular.every_day(
-        Box::new(move || Box::pin(my_runner())),
-        Some(DailyTaskOptoins {
-            day: None,
-            super_struct: TaskOptions {
-                should_run_late: false,
-                name: None,
-                time: 0,
-            },
-        }),
-    );
-
-    my_schedular.every_day(
-        Box::new(move || Box::pin(my_runner1())),
-        Some(DailyTaskOptoins {
-            day: None,
-            super_struct: TaskOptions {
-                should_run_late: false,
-                name: None,
-                time: 37800,
-            },
-        }),
-    );
-
-    my_schedular.every_day(
-        Box::new(move || Box::pin(my_runner2())),
-        Some(DailyTaskOptoins {
-            day: None,
-            super_struct: TaskOptions {
-                should_run_late: false,
-                name: None,
-                time: 10,
-            },
-        }),
-    );
     my_schedular.start().await;
 }
