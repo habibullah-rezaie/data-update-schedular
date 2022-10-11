fd7c9100e7a017e9644372024da3ff0733eb866d
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-11 13:43:40 +0500

    implement the function `difference_in_secs_from_now`

--- src/util/time.rs
+++ src/util/time.rs
@@ -1,7 +1,21 @@
+use std::io::Error;
+
 use chrono::{NaiveTime, Utc};
 
-pub fn diference_in_secs_from_now(secs: usize) -> usize {
-    let now = (Utc::now().timestamp() / 1000) as usize;
+/// Get the difference of a time in during the day from now
+///
+/// * `time_seconds` - The time of day in seconds
+/// NOTE: time_seconds cannot be more than 87400
+pub fn get_diff_from_now_in_secs(time_seconds: u32) -> std::result::Result<i64, Error> {
+    let now = NaiveTime::from_num_seconds_from_midnight_opt(time_seconds, 0);
 
-    return now - secs;
+    if now.is_none() {
+        return Err(Error::new(
+            std::io::ErrorKind::Other,
+            "Invalid `time_seconds` provided",
+        ));
+    }
+    let diff = NaiveTime::signed_duration_since(now.unwrap(), Utc::now().time()).num_seconds();
+
+    return Ok(diff);
 }
