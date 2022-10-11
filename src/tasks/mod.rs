0ea59507b754cef646a9d5b01b537781a4de9778
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-11 12:47:22 +0500

    Create `get_tasks` to retrieve all tasks

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -1,1 +1,25 @@
 mod domy_task;
+
+use chrono::Weekday;
+
+use crate::{
+    schedular::{
+        schedular::TaskRunner,
+        task::task_options::{DailyTaskOptoins, TaskOptions},
+    },
+};
+
+pub fn get_tasks() -> Vec<(TaskRunner, DailyTaskOptoins)> {
+    return vec![(
+        Box::new(move || Box::pin(domy_task::domy_task_runner())),
+        DailyTaskOptoins {
+            // As an example the task should be run on every Tuesday at 00:00:00
+            day: Some(Weekday::Tue),
+            super_struct: TaskOptions {
+                should_run_late: true,
+                name: Some("Domy_task".to_string()),
+                time: 0,
+            },
+        },
+    )];
+}
