9512bd69a06104ffe0607e6269a8c104bd175f0a
Habibullah Rezaie <habibullah.rezaie.8@gmail.com>
2022-10-11 16:00:11 +0500

    Bring `Task` struct to `mod.rs`

--- src/schedular/task/mod.rs
+++ src/schedular/task/mod.rs
@@ -1,3 +1,23 @@
-pub mod task;
 pub mod task_options;
 pub mod types;
+
+use uuid::Uuid;
+
+use self::{task_options::TaskOptions, types::TaskTime};
+
+#[derive(Debug, Clone)]
+pub struct Task {
+    pub name: String,
+    pub should_run_late: bool,
+    pub time: TaskTime,
+}
+
+impl Task {
+    pub fn new(options: TaskOptions) -> Self {
+        Task {
+            name: options.name.unwrap_or_else(|| Uuid::new_v4().to_string()),
+            should_run_late: options.should_run_late,
+            time: options.time,
+        }
+    }
+}
